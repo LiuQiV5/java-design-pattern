e3d317fd4b66c21c962c0b4dcd52167db22a68fa
LiuQiV5 <1504885830@qq.com>
2019-02-07 22:50:31 +0800

    设计模式之享元模式

---
+++ src/main/java/com/liuqi/v15/BlackIgoChessman.java
@@ -0,0 +1,11 @@
+package com.liuqi.v15;
+
+/**
+ * 黑色棋子类：具体享元类
+ */
+public class BlackIgoChessman extends IgoChessman {
+    @Override
+    public String getColor() {
+        return "黑色";
+    }
+}
